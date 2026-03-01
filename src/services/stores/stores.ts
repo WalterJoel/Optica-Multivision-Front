1308171ef3e747a15deaab00965e0aeef1348307
WalterJoel <walter.valdivia@ucsp.edu.pe>
2026-03-01 12:25:41 -0500

    s

--- src/services/stores/stores.ts
+++ src/services/stores/stores.ts
@@ -1,7 +1,7 @@
 import { api } from "../api";
-import { CreateStore } from "@/types/stores";
+import { ICreateStore } from "@/types/stores";
 
-export const createStoreService = async (payload: CreateStore) => {
+export const createStoreService = async (payload: ICreateStore) => {
   const { data } = await api.post("/sedes/crearSede", payload);
   return data;
 };
