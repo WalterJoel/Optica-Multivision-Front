171167c69fbbff77bcfb32cd9046cc320a82e27d
WalterJoel <walter.valdivia@ucsp.edu.pe>
2026-04-03 13:45:51 -0500

    s

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   // token-only => no cookies
-  withCredentials: false,
+  withCredentials: true,
 });
 
 // ✅ Enviar token en cada request
