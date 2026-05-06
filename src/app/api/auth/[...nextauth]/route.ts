6363b8742bf18bb69296159a4eab827ab1b30636
WalterJoel <walter.valdivia@ucsp.edu.pe>
2026-05-05 19:53:21 -0500

    s

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,11 +6,11 @@ async function loginUser(
   email: string,
   password: string,
 ): Promise<LoginResponse> {
-  if (!process.env.LOGIN_URL) {
-    throw new Error("LOGIN_URL no está definido");
+  if (!process.env.NEXT_PUBLIC_LOGIN_URL) {
+    throw new Error("NEXT_PUBLIC_LOGIN_URL no está definido");
   }
 
-  const res = await fetch(process.env.LOGIN_URL, {
+  const res = await fetch(process.env.NEXT_PUBLIC_LOGIN_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
